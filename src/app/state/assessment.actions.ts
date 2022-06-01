a4736e1ee9f5ae3bb7d97ed541bf599a65be9ed6
Your Namefater tobi <oluwatobi.fateru@titantrustbank>
2022-06-01 16:03:27 +0100

    implementing state

---
+++ src/app/state/assessment.actions.ts
@@ -0,0 +1,6 @@
+import { createAction } from "@ngrx/store";
+
+export const loadCharacter = createAction('loadCharacter');
+export const loadLocation = createAction('loadLocation')
+export const loadEpisode = createAction('loadEpisode');
+export const changeSearch = createAction('changeSearch');
---
+++ src/app/state/assessment.state.ts
@@ -0,0 +1,12 @@
+export const characterState = {
+    counter: 0
+}
+export const locationState = {
+    characters: null
+}
+export const episodeState = {
+    characters: null
+}
+export const searchState = {
+    characters: null
+}
